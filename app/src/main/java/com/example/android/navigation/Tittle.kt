941677b9eb5a385d01e20dc494cfa822b033723d
Fachrul <ffaathirullah1@gmail.com>
2020-09-07 17:29:52 +0700

    Navigate

--- app/src/main/java/com/example/android/navigation/Tittle.kt
+++ app/src/main/java/com/example/android/navigation/Tittle.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
 import com.example.android.navigation.databinding.FragmentTitleBinding
 
 
@@ -14,6 +15,7 @@ class Tittle : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         val binding = DataBindingUtil.inflate<FragmentTitleBinding>(inflater, R.layout.fragment_title, container, false)
+        binding.playButton.setOnClickListener {view:View -> view.findNavController().navigate(R.id.action_myNavHostFragment_to_gameFragment2) }
         return binding.root
     }
 }
